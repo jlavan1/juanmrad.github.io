8081687b8b833f59c832175aea118a59b47c354a
Juan Mrad <juansmrad@gmail.com>
2019-11-05 19:56:52 -0600

    watchlist page test

--- watchlist.js
+++ watchlist.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     var watchListJSON = localStorage.getItem("watchlist");
     var watchlist = JSON.parse(watchListJSON);
+    console.log(watchlist);
 
-    function renderMovies(movieArray) {
-        var movieHTML = movieArray.map(function (currentMovie){
-            return `
-                <div class="movies-container col-3">
-					<div class="card text-white" style="width: 18rem;">
-						<img src="${currentMovie.Poster}" class="card-img-top" alt="Card image">
-						<div class="card-body">
-							<h5 class="card-title">${currentMovie.Title}</h5>
-							<p class="card-text">${currentMovie.Year}</p>
-							<button onclick="saveToWatchlist('${currentMovie.imdbID}')" class="btn btn-primary">Add</a></button>
+  function renderMovies(movieArray) {
+    var movieHTML = movieArray.map(function (currentMovie){
+      return `
+          <div class="movie">
+							<div class="card" style="width: 18rem;">
+									<img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">${currentMovie.Title} <span class="badge badge-secondary">${currentMovie.Year}</span></h5>
+										<p class="card-text">IMDB ID: ${currentMovie.imdbID}</p>
+										<button onclick="saveToWatchlist('${currentMovie.imdbID}')" class="btn btn-primary">Go somewhere</button>
+									</div>
 						</div>      
-					</div>
-				</div>
-        `;
-        });
+									</div>
+							</div>
+          </div>
+          `;
+    });
 
-        return movieHTML.join("");
-    }
+    return movieHTML.join('');
+  }
 
-    var content = document.getElementById("movies-container");
-    content.innerHTML = renderMovies(watchlist);
-})
+  var content = document.getElementById('movies-container');
+  content.innerHTML = renderMovies(watchlist);
+});
